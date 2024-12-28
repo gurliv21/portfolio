3ac513c488a24a7eb45537cd1eac220ce6797222
gurlivkaurbajwa <gurlivbajwa@gmail.com>
2024-12-28 22:13:49 +0530

    second commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import img2 from "/Users/gurlivkaurbajwa/Desktop/portfolio/src/asserts/img2.png";
-import img3 from "/Users/gurlivkaurbajwa/Desktop/portfolio/src/asserts/img3.png";
-import img4 from "/Users/gurlivkaurbajwa/Desktop/portfolio/src/asserts/img4.png";
-import img5 from "/Users/gurlivkaurbajwa/Desktop/portfolio/src/asserts/img5.png";
-import img1 from "/Users/gurlivkaurbajwa/Desktop/portfolio/src/asserts/img1.png";
+import img2 from "../asserts/img2.png";
+import img3 from "../asserts/img3.png";
+import img4 from "../asserts/img4.png";
+import img5 from "../asserts/img5.png";
+import img1 from "../asserts/img1.png";
 import Header from "./Header";
 
 const Project = () => {
@@ -17,28 +17,32 @@ const Project = () => {
 
   return (
     <div className="h-screen overflow-auto">
-      <Header/>
-    <div className="container mx-auto px-6 py-16 ">
-      
-      <h1 className="text-5xl font-extrabold  mb-12 text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-blue-600">
-        Projects.
-      </h1>
-      <div className="grid grid-cols-1 gap-8">
-        {images.map((image, index) => (
-          <div key={index} className="flex flex-row bg-white/30 backdrop-blur-lg p-8 rounded-2xl ">
-            <img 
-              src={image.src} 
-              alt={`Project ${index + 1}`} 
-              className="w-96 h-64 object-cover rounded-lg mb-4 border-2 shadow-lg hover:shadow-xl transition-all duration-300 "
-            />
-            <div>
-              <h2 className="text-2xl font-semibold text-center ">{image.title}</h2>
-              <p className="text-center  mt-2">{image.des}</p>
+      <Header />
+      <div className="container mx-auto px-6 py-16 ">
+        <h1 className="text-5xl font-extrabold mb-12 text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-blue-600">
+          Projects.
+        </h1>
+        <div className="grid grid-cols-1 gap-8">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="flex flex-row bg-white/30 backdrop-blur-lg p-8 rounded-2xl"
+            >
+              <img
+                src={image.src}
+                alt={`Project ${index + 1}`}
+                className="w-96 h-64 object-cover rounded-lg mb-4 border-2 shadow-lg hover:shadow-xl transition-all duration-300"
+              />
+              <div>
+                <h2 className="text-2xl font-semibold text-center">
+                  {image.title}
+                </h2>
+                <p className="text-center mt-2">{image.des}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
